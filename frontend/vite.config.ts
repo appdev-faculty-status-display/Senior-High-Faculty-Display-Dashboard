3077248b3839b1aaf817f304bbf102b52877fc63
sonickysh <keithyvesrobles@gmail.com>
2026-05-09 08:37:13 +0800

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
-    setupFiles: ['./src/setup.ts'],
+    setupFiles: ['./src/test/setup.ts'],
   },
   resolve: { 
     alias: { 
